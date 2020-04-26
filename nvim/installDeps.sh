7599ca6198e288c37d422aa2d91d258bd147b898
pseydel <asderuiner@gmail.com>
2020-04-26 18:39:52 +0200

    Added PATH enhancements

--- nvim/installDeps.sh
+++ nvim/installDeps.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #declare -A debian_PKG
-DEBIAN_PKG="git ranger neovim lua5.3 npm chromium-browser chromium-codecs-ffmpeg valgrind gcc clang clangd python3"
+DEBIAN_PKG="git ranger neovim lua5.3 npm chromium-browser chromium-codecs-ffmpeg valgrind gcc clang clangd python3 python3-pip openssh-server openssh-client"
 
 #declare -A arch_PKG
-ARCH_PKG="git ranger neovim lua npm chromium valgrind gcc clang python"
+ARCH_PKG="git ranger neovim lua npm chromium valgrind gcc clang python python-pip openssh"
 
 #declare -A ALPINE_PKG
 ALPINE_PKG="git ranger neovim lua python3 npm chromium"
@@ -17,12 +17,27 @@ SUSE_PKG="git ranger neovim lua python3 npm chromium"
 #declare -A GENTOO_PKG
 GENTOO_PKG="git ranger neovim lua python3 npm chromium"
 
-echo $DEBIAN_PKG
+
+PYTHON_PKG="autopep8 McCabe python-language-server"
 
 
 if [ -x "$(command -v apk)" ]; then apk add --no-cache $ALPINE_PKG;
-elif [ -x "$(command -v pacman)" ]; then pacman -S --noconfirm $ARCH_PKG; 
-elif [ -x "$(command -v yum)" ]; then echo $REDHAT_PKG; 
-elif [ -x "$(command -v emerge)" ]; then echo $GENTOO_PKG; 
-elif [ -x "$(command -v zypp)" ]; then echo $SUSE_PKG;
-elif [ -x "$(command -v apt)" ]; then apt install --yes $DEBIAN_PKG; fi
+elif [ -x "$(command -v pacman)" ]; then 
+    pacman -S --noconfirm $ARCH_PKG;
+    systemctl enable sshd.service && systemctl start sshd.service;
+    python -m pip install --user $PYTHON_PKG
+elif [ -x "$(command -v yum)" ]; then 
+    echo $REDHAT_PKG; 
+elif [ -x "$(command -v emerge)" ]; then 
+    echo $GENTOO_PKG; 
+elif [ -x "$(command -v zypp)" ]; then 
+    echo $SUSE_PKG;
+elif [ -x "$(command -v apt)" ]; then 
+    apt install --yes $DEBIAN_PKG;
+    systemctl enable ssh.service && systemctl start ssh.service
+    python3 -m pip install --user $PYTHON_PKG
+fi
+
+ENV_PATH=/etc/environment
+ENV_INNER=$(cat $ENV_PATH)
+echo ${ENV_INNER%\"}:\$HOME/.local/bin\" > $ENV_PATH
